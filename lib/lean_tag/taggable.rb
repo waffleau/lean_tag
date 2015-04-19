b021b080f6bceb0752f63a0820b0e9d154ebc9d4
Matt Ellis <m.ellis27@gmail.com>
2015-04-19 19:30:39 +1000

    Fixes to removal of tags without immediate save

--- lib/lean_tag/taggable.rb
+++ lib/lean_tag/taggable.rb
@@ -6,6 +6,8 @@ module LeanTag
         has_many :taggings, class_name: "LeanTag::Tagging", as: :record, inverse_of: :record, dependent: :destroy
         has_many :tags, through: :taggings
 
+        accepts_nested_attributes_for :taggings, allow_destroy: true
+
         scope :with_tags, -> { includes(:tags) }
       end
     end
@@ -45,23 +47,9 @@ module LeanTag
     end
 
     ##
-    # Destroy a tag if it's no longer in use
-    def destroy_if_unused(tag)
-      if tag.taggings_count && LeanTag.config.remove_unused
-        tag.destroy
-      end
-    end
-
-    ##
     # Finds current tags on this record which aren't in the passed list
     def excluded_tags(tag_names)
       self.tags.reject { |t| t.name.in?(tag_names) }
-    end
-
-    ##
-    # Finds a tag on this record by name
-    def find_tag(tag_name)
-      self.tags.find_by_name(tag_name)
     end
 
     ##
@@ -72,30 +60,30 @@ module LeanTag
 
     ##
     # Removes a single tag on parent save
-    def remove_tag(tag, method='mark_for_destruction')
-      tag = self.find_tag(tag) if tag.is_a?(String)
+    def remove_tag(tag)
+      tag = self.tags.where(name: tag).first if tag.is_a?(String)
 
-      tagging = tagging.find_by_tag_id(tag.id)
-      tagging.send(method) unless tagging.nil?
-
-      destroy_if_unused(tag)
+      self.taggings.each { |t| t.mark_for_destruction if t.tag.eql?(tag) }
     end
 
     ##
     # Removes a single tag immediately
     def remove_tag!(tag)
-      tag = self.find_tag(tag) if tag.is_a?(String)
+      tag = self.tags.where(name: tag).first if tag.is_a?(String)
 
-      tagging = tagging.find_by_tag_id(tag.id)
-      tagging.destroy unless tagging.nil?
-
-      destroy_if_unused(tag)
+      self.taggings.each { |t| t.mark_for_destruction if t.tag.eql?(tag) }
     end
 
     ##
     # Set a list of tags
     def tag_list=(value)
-      tag_names = value.blank? ? [] : value.split(LeanTag.config.delimiter)
+      if value.is_a?(String)
+        tag_names = value.split(LeanTag.config.delimiter)
+      elsif value.is_a?(Array)
+        tag_names = value
+      else
+        tag_names = []
+      end
 
       # Get rid of existing tags that aren't in the list
       self.excluded_tags(tag_names).each { |t| self.remove_tag(t) }
@@ -107,7 +95,7 @@ module LeanTag
     ##
     # Returns a delimited list of tag names
     def tag_list
-      self.tags.map(&:name).join(LeanTag.config.delimiter)
+      self.tags.map(&:name).join(',')
     end
 
   end
--- lib/lean_tag/version.rb
+++ lib/lean_tag/version.rb
@@ -1,5 +1,5 @@
 module LeanTag
 
-  VERSION = '0.1.3'
+  VERSION = '0.1.5'
 
 end
